3d22572880bfb062427c390e5952efb0832b65c9
mars <mars2022@vip.qq.com>
2018-03-26 11:58:40 +0800

    add common class

--- src/gooalcharts.js
+++ src/gooalcharts.js
@@ -1,6 +1,7 @@
 import * as d3 from 'd3';
 
 export default class GooalCharts {
+
     constructor(dom, options) {
         this.width = options.width;
         this.height = options.height;
@@ -11,27 +12,64 @@ export default class GooalCharts {
         this.dataBox = this.dataBoxInit(options.drawBox);
     }
 
+    getWidth() {
+        return this.width;
+    }
+
+    getHeight() {
+        return this.height;
+    }
+
+    // container
     containerInit(dom) {
         var container = d3.select(dom)
             .append("svg")
             .attr("width", this.width)
-            .attr("height", this.height);
+            .attr("height", this.height)
+            .attr("class", "container");
         return container;
     }
+    getContainer() {
+        return this.container;
+    }
 
+    // titlebox
     titleBoxInit(titlebox) {
-
+        var titleBox;
+        return titleBox;
     }
 
+    getTitleBox() {
+        return this.titleBox;
+    }
+
+    // axisbox
     axisBoxInit(axisbox) {
-
+        var axisBox;
+        return axisBox;
     }
 
+    getAxisBox() {
+        return this.axisBox;
+    }
+
+    // legend box
     legendBoxInit(legendbox) {
-
+        var legendBox;
+        return legendBox;
     }
 
-    dataBoxInit(drawbox) {
+    getLegendBox() {
+        return this.legendBox;
+    }
 
+    // databox
+    dataBoxInit(drawbox) {
+        var drawBox;
+        return drawBox;
+    }
+
+    getDataBox() {
+        return this.dataBox;
     }
 };

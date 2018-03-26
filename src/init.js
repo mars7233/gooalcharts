980bd660fb9034511cbc2c72ece7751c151df09c
mars <mars2022@vip.qq.com>
2018-03-26 11:56:32 +0800

    new initialize method

--- src/init.js
+++ src/init.js
@@ -1,21 +1,18 @@
 import * as d3 from 'd3';
 import GooalCharts from './gooalcharts';
-import column from "./gooal-column";
+import GooalColumn from './gooalcolumn';
 
 var containerWidth, containerHeight = 0;
-var container = ""
 var titleBox, axisBox, legendBox, drawBox = "";
 var chartType = ""
 
 // 初始化入口
 function chartsInit(dom, options) {
-    // 初始化容器
-    container = new GooalCharts(dom, options).container;
-
     // 判断图表类型
     chartType = options.type;
     if (chartType == "column") {
-        column(container, options);
+        var column = new GooalColumn(dom, options);
+        column.draw(dom, options);
     }
 }
 

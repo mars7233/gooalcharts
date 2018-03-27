8d0f6a63641c17f669507f56d4ef58538dda765a
mars <mars2022@vip.qq.com>
2018-03-27 21:46:43 +0800

    add return element

--- src/init.js
+++ src/init.js
@@ -1,19 +1,21 @@
 import * as d3 from 'd3';
 import GooalCharts from './gooalcharts';
-import GooalColumn from './gooalcolumn';
+import GooalBar from './bar/gooalbar';
 
 var containerWidth, containerHeight = 0;
 var titleBox, axisBox, legendBox, drawBox = "";
 var chartType = ""
+var chart;
 
 // 初始化入口
 function chartsInit(dom, options) {
     // 判断图表类型
     chartType = options.type;
-    if (chartType == "column") {
-        var column = new GooalColumn(dom, options);
-        column.draw(dom, options);
+    if (chartType == "bar") {
+        chart = new GooalBar(dom, options);
+        chart.draw(dom, options);
     }
+    return chart;
 }
 
 export default function (dom, options) {

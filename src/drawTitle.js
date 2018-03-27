b2295a755177f80e7b6c032d933210083c89a385
mars <mars2022@vip.qq.com>
2018-03-27 15:06:42 +0800

    add title function

--- src/drawTitle.js
+++ src/drawTitle.js
@@ -1,7 +1,14 @@
 import * as d3 from 'd3';
 
 function drawTitle(dom, options) {
-
+    var svg = dom;
+    console.log(dom);
+    svg.append("text")
+        .attr("x", 400)
+        .attr("y", 20)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Title");
 }
 
 export default function (dom, options) {

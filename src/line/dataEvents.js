b2fbdfab835eb5418665c2f0030b992f9ab09d6f
wirelessmice <963999154@qq.com>
2018-05-04 10:26:34 +0800

    添加曲线图

--- src/line/dataEvents.js
+++ src/line/dataEvents.js
@@ -1,10 +1,37 @@
 import * as d3 from 'd3'
+import { getObjValue } from '../tools/gooalArray';
 
 let commonOpt
 let data
 
 function handleLineData(opt) {
+    commonOpt = opt
+    // 绑定数据
+    data = commonOpt.data
 
+    var dataSet = []
+    data.forEach(element => {
+        let x = getObjValue(0, element)
+        let y = getObjValue(1, element)
+        var d = []
+        d.push(x)
+        d.push(y)
+        dataSet.push(d)
+    });
+
+    dataSet.sort(function(a, b){
+        return a[0] - b[0]
+    });
+
+    let ds = []
+    dataSet.forEach(element => {
+        var x = element[0]
+        var y = element[1]
+        ds.push({"x": x,"y": y})
+        
+    });
+
+    return ds
 }
 
 export { handleLineData }

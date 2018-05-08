1ad896fcd31c7f8e317522cbee8766fbb0e2840c
mars <mars2022@vip.qq.com>
2018-05-08 11:16:31 +0800

    Add：增加双击改Title功能

--- src/chartEvent/mouseEvent.js
+++ src/chartEvent/mouseEvent.js
@@ -7,13 +7,13 @@ let hoverColor = "brown"
 let commonOpt
 let selectedData = []
 
-function addEvents(svg, events, methods, opt) {
-    commonOpt = opt
+function addEvents(svg, events, methods, options) {
+    commonOpt = options
     chartEl = svg
-    chartEl.selectAll("." + opt.type + "Element" + opt.id)
+    chartEl.selectAll("." + options.type + "Element" + options.id)
         .on(events, methods)
 }
-// default events
+// 默认事件（）
 function defaultEvents(svg, options) {
     // options  鼠标悬浮颜色、大小
     commonOpt = options
@@ -30,6 +30,49 @@ function defaultEvents(svg, options) {
     chartEl.selectAll("." + commonOpt.type + "Element" + commonOpt.id)
         .on("mouseover.highlight", mouseOverHighlight)
         .on("mouseout.highlight", handleMouseOut)
+}
+// 双击改标题
+function dbClickChangeTitle(svg, options) {
+    commonOpt = options
+    let titleBox = d3.select("#" + options.type + "TitleBox" + options.id)
+    svg.on("dblclick", function () {
+        let title = svg.text()
+        let titleBBox = svg.node().getBBox()
+        svg.text("")
+
+        let foreignObject = titleBox.append("foreignObject")
+            // .attr("x", titleBBox.x - 3)
+            // .attr("y", -2)
+            .attr("width", "90%")
+            .attr("height", "100%")
+            .style("align", "center")
+            .html("<input class = " + options.type + "TitleChange" + options.id + "  type = \"text\" >")
+
+        let inputLabel = d3.select("." + options.type + "TitleChange" + options.id)
+            .attr("value", title)
+            .attr("text-anchor", "middle")
+            .style("font-family", "Times")
+            .style("font-size", "21px")
+            .attr("text-align", "center")
+
+        inputLabel.style("width", "100%")
+            .style("x", titleBBox.x - 3)
+            .style("y", -2)
+
+        inputLabel.on("keydown", function () {
+            if (d3.event.key == "Enter") {
+                inputLabel.remove()
+                foreignObject.remove()
+                svg.attr("x", "50%")
+                    .attr("y", 20)
+                    .text(inputLabel.node().value)
+                inputLabel.on("keydown", null)
+            }
+        })
+
+
+    })
+
 }
 
 // mouse over
@@ -79,9 +122,6 @@ function handleClickOutside(options) {
     }
 }
 
-export { addEvents }
-export { defaultEvents }
-export { selectEvent }
 export default class MouseEvent {
     constructor() {
 
@@ -93,23 +133,23 @@ export default class MouseEvent {
             .on("mouseover.highlight", null)
             .on("mouseout.highlight", null)
         handleClickOutside(commonOpt)
-        if (method == "single") {
+        if (method == "single") {//单选事件
             selectedData = null
             restoreColor()
             cleanSelectEvent()
             chartEl.selectAll("." + commonOpt.type + "Element" + commonOpt.id)
                 .on("click.singleSelect", function (d, i) {
-                    if (d3.select(this).style("fill") == selectColor) {
+                    if (d3.select(this).style("fill") == selectColor) {// 如果元素已被选中则取消选择
                         restoreColor()
                         selectedData = null
-                    } else {
+                    } else {// 如果元素未被选中则选择
                         restoreColor()
                         d3.select(this).style("fill", selectColor)
                         selectedData = d
                         console.log(selectedData)
                     }
                 })
-        } else if (method == "multiple") {
+        } else if (method == "multiple") {//多选事件
             selectedData = []
             restoreColor()
             cleanSelectEvent()
@@ -117,15 +157,13 @@ export default class MouseEvent {
                 .on("click.multiSelect", function (d, i) {
                     let normalColor = d3.select(this).attr("normalColor")
                     let overlapFlag = false
-                    if (selectedData.length == 0) {
-                        // 如果选择的元素集为空，则把该元素加入选择集中
+                    if (selectedData.length == 0) {// 如果选择的元素集为空，则把该元素加入选择集中
                         selectedData.push(d)
                         d3.select(this).style("fill", selectColor)
                     } else {
                         let count = 0
                         for (let element of selectedData) {
-                            if (d == element) {
-                                // 如果选择集内元素重复，则删除该元素
+                            if (d == element) {// 如果选择集内元素重复，则删除该元素
                                 overlapFlag = true
                                 selectedData.splice(count, 1)
                                 d3.select(this).style("fill", normalColor)
@@ -133,8 +171,7 @@ export default class MouseEvent {
                             }
                             count++
                         }
-                        if (overlapFlag == false) {
-                            // 如果选择集中没有当前选中元素，则把元素加入选择集中
+                        if (overlapFlag == false) {// 如果选择集中没有当前选中元素，则把元素加入选择集中
                             selectedData.push(d)
                             d3.select(this).style("fill", selectColor)
                         }
@@ -155,3 +192,9 @@ export default class MouseEvent {
     }
 
 }
+
+
+export { addEvents }
+export { defaultEvents }
+export { selectEvent }
+export { dbClickChangeTitle }

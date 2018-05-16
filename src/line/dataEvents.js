4161dcd22ffd5d70a7e4c9bb0ca34f2212f4f9f7
wirelessmice <963999154@qq.com>
2018-05-16 19:49:22 +0800

    增加曲线图

--- src/line/dataEvents.js
+++ src/line/dataEvents.js
@@ -34,4 +34,34 @@ function handleLineData(opt) {
     return ds
 }
 
-export { handleLineData }
+function handleCurveLineData(opt) {
+    commonOpt = opt
+    // 绑定数据
+    data = commonOpt.data
+
+    var dataSet = []
+    data.forEach(element => {
+        let x = getObjValue(0, element)
+        let y = getObjValue(1, element)
+        var d = []
+        d.push(x)
+        d.push(y)
+        dataSet.push(d)
+    });
+
+    dataSet.sort(function (a, b) {
+        return a[0] - b[0]
+    });
+
+    let ds = []
+    dataSet.forEach(element => {
+        var x = element[0]
+        var y = element[1]
+        ds.push({ "x": x, "y": y })
+
+    });
+
+    return ds
+}
+
+export { handleLineData, handleCurveLineData }
--- src/line/linePresenter.js
+++ src/line/linePresenter.js
@@ -1,7 +1,9 @@
 import drawLine from './lineView'
+import drawCurveLine from './lineViewCurve'
 import { defaultEvents as mouseDefault } from './mouseEvents'
-import { handleLineData } from './dataEvents'
+import { handleLineData, handleCurveLineData } from './dataEvents'
 import drawAxis from '../drawAxis'
+import { handleStackedBar } from '../bar/dataEvents';
 
 let width = 800
 let height = 400
@@ -26,11 +28,20 @@ function presenter(dom, options, legendDom, newWidth) {
     // 容器
     lineContainer = dom
 
+    // 折线图
     if (options.type == "linechart") {
         let lineChart
         data = handleLineData(options)
         lineChart = drawLine(lineContainer, data, options, newWidth)
         drawAxis(lineChart, options, newWidth)
+
+
+      // 曲线图 
+    } else if (options.type == "curvelinechart") {
+        let curvelinechart
+        data = handleCurveLineData(options)
+        curvelinechart = drawCurveLine(lineContainer, data, options, newWidth)
+        drawAxis(curvelinechart, options, newWidth)
     }
     return lineContainer
 }
